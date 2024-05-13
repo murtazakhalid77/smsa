3abacb22c3b561a5404509842817003b7cbbcc5e
ahmedtariqkhan <ahmedkhansaduzai@gmail.com>
2024-05-14 00:42:13 +0500

    Awb's to Text Conversion Temporary Implementation

--- src/main/java/com/smsa/backend/controller/TextController.java
+++ src/main/java/com/smsa/backend/controller/TextController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api")
+@RequestMapping("/api/download")
 public class TextController {
 
     @Autowired
