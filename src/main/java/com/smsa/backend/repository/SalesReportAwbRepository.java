0c4461fd22664a07085b6b737f706318dc79b1c5
ahmedtariqkhan <ahmedkhansaduzai@gmail.com>
2025-03-25 21:02:28 +0500

    - Removed Extra's

--- src/main/java/com/smsa/backend/repository/SalesReportAwbRepository.java
+++ src/main/java/com/smsa/backend/repository/SalesReportAwbRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 
 @Repository
 public interface SalesReportAwbRepository extends JpaRepository<SalesReportAwb, Long> {
-    SalesReportAwb findBySalesReport(SalesReport salesReport);
+
+
 }
